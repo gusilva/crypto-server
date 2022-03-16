41440e51e9d0570306dc891e6b2c1df482356cf0
Gustavo Silva <silva.gustavo@mercurydevelopment.com>
2022-03-16 21:11:54 +0300

    Add coin module

--- src/coins/coin.module.ts
+++ src/coins/coin.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoinController } from './coin.controller';
 import { CoinService } from './coin.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CoinSchema } from './coin.schema';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: 'Coin', schema: CoinSchema }])],
   controllers: [CoinController],
   providers: [CoinService],
 })
